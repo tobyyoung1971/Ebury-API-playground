8645043cb842035b046f06edb36969d6c7cc9659
Toby Young <tobyyoung@hotmail.com>
2025-02-25 17:23:35 +0400

    Added some documentation to the config.py file

--- flask-ebury-callback-app/app/config.py
+++ flask-ebury-callback-app/app/config.py
@@ -1,10 +1,19 @@
 class Config:
+    # only needed for host to host
+    # not needed it you use the ebo login function
     EBURY_USERNAME = "email"
     EBURY_PASSWORD = "password"
+    
+    # The next three of config variables must be included and must match with those
+    # held in the environment you are connecting to 
     EBURY_AUTH_CLIENT_ID= 'client auth id'
     EBURY_AUTH_CLIENT_SECRET= 'client secret'
     EBURY_REDIRECT_URL = "your OpenID connect redirect url"
+    # the redirect for the ebo login to work must be
+    # EBURY_REDIRECT_URL = "http://127.0.0.1:5000/auth_callback"
+    # for this app to work locally
 
+    # These are the urls for the environment to connect to
     EBURY_AUTHENTICATION_URL = "https://auth-sandbox.ebury.io/"
     EBURY_API_URL = "https://sandbox.ebury.io/"
 
