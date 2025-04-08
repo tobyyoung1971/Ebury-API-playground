ac6c97ba274dcda1f2ed8855d2f0a6ee4663536d
Toby Young <tobyyoung@hotmail.com>
2025-04-08 11:08:11 +0100

    Update ip and port to ensure localhost and 127.0.0.1 both resolve

--- flask-ebury-callback-app/run.py
+++ flask-ebury-callback-app/run.py
@@ -4,4 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
    # app.run(debug=True)
-   socketio.run(app, debug=True, host='127.0.0.1', port=5000)
+   # Using 0.0.0.0 to ensure resolution to both localhost and 127.0.0.1
+   socketio.run(app, debug=True, host='0.0.0.0', port=5001)
